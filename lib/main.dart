7da01d7c2960a28527879c49cf9fdb9def952b80
kapil jhajhria <jhajhria44@gmail.com>
2019-10-02 18:19:15 +0530

    Added Ce1 and Ce2
    General Box arrangement so far
    Row1
     E1  E2
     E1 has Column with 4 containers
     E2 has picture of desert -Almost done
    
     E1
     Column
      Ce1 Desert Name - Almost done
      Ce2 Description - Almost done
      Ce3 review and rating - Almost done
      Ce4 Icons with some details
    
      Created Ce3 for now
      Ce3 has
       Row(with another row which has 5 start icon)
       Container(Text widget with number of reviews)

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,47 @@ class MyApp extends StatelessWidget {
               flex: 1,
               child: Column(
                 children: <Widget>[
-                  Container(),
-                  Container(),
                   Container(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.blue.shade100,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Strawberry pavlova",
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue.shade100,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+//                    width: 200.0,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 4.0, horizontal: 10.0),
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: Center(
+                      child: Text(
+                        "pavlova is a meringue-based"
+                        " dessert names after the Russian"
+                        " ballerine Arina Pavlova."
+                        "Pavlova features a crisp crust and "
+                        "soft, light inside, topped with fruit"
+                        "and whipped cream. ",
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
                     decoration: BoxDecoration(
                       color: Colors.blue.shade100,
                       border: Border.all(
