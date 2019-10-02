67dfdcc1a7ad24d6e6669f5c6069ed0198f97f46
kapil jhajhria <jhajhria44@gmail.com>
2019-10-02 18:06:59 +0530

    Review Container firts
    Created
    Row1
     E1  E2
     E1 has Column with 4 containers
     E2 has picture of desert
    
     E1
     Column
      Ce1 Desert Name
      Ce2 Description
      Ce3 review and rating
      Ce4 Icons with some details
    
      Created Ce3 for now
      Ce3 has
       Row(with another row which has 5 start icon)
       Container(Text widget with number of reviews)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,76 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Strawberry Pavlova'),
-      ),
-      body: Container(),
-    ),
-  ));
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Strawberry Pavlova'),
+        ),
+        body: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: <Widget>[
+                  Container(),
+                  Container(),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue.shade100,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.star,
+                              size: 14,
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 14,
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 14,
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 14,
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 14,
+                            ),
+                          ],
+                        ),
+                        Container(
+                          child: Text('170 Reviews'),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Image(
+                image: AssetImage('image/food.png'),
+                fit: BoxFit.contain,
+              ),
+            ),
+          ],
+        ));
+  }
 }
